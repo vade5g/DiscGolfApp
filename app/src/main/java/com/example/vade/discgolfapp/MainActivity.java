035b6241313ecd14b9af532756a76e3365f26ecf
vade5g <valtteri.kukkola@gmail.com>
2018-05-09 23:55:04 +0100

    Changes for the UI

--- app/src/main/java/com/example/vade/discgolfapp/MainActivity.java
+++ app/src/main/java/com/example/vade/discgolfapp/MainActivity.java
@@ -18,9 +18,12 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -30,9 +33,32 @@ public class MainActivity extends AppCompatActivity {
     private static List<game> games;
     private static List<course> courses;
 
-    private Button plusButton1;
-    private Button minusButton1;
+    //TextViews
+
     private TextView scoreTW1;
+    private TextView scoreTW2;
+    private TextView scoreTW3;
+    private TextView scoreTW4;
+    private TextView scoreTW5;
+    private TextView scoreTW6;
+    private TextView scoreTW7;
+    private TextView scoreTW8;
+    private TextView scoreTW9;
+    private TextView scoreTW10;
+    private TextView scoreTW11;
+    private TextView scoreTW12;
+    private TextView scoreTW13;
+    private TextView scoreTW14;
+    private TextView scoreTW15;
+    private TextView scoreTW16;
+    private TextView scoreTW17;
+    private TextView scoreTW18;
+
+
+
+    private static Map<Integer, Integer> idPlusMap = new HashMap<Integer, Integer>();
+    private static Map<Integer, Integer> idMinusMap = new HashMap<Integer, Integer>();
+    private static Map<Integer, Integer> idTWMap = new HashMap<Integer, Integer>();
 
     private AppDatabase mDb;
 
@@ -40,12 +66,73 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         testTextView = findViewById(R.id.testTextView);
-        plusButton1 = findViewById(R.id.buttonPlus1);
-        minusButton1 = findViewById(R.id.buttonMinus1);
-        scoreTW1 = findViewById(R.id.Score1TW);
-        scoreTW1.setText("3");
+        //map all the plus buttons.
+        idPlusMap.put(R.id.buttonPlus1,1);
+        idPlusMap.put(R.id.buttonPlus2,2);
+        idPlusMap.put(R.id.buttonPlus3,3);
+        idPlusMap.put(R.id.buttonPlus4,4);
+        idPlusMap.put(R.id.buttonPlus5,5);
+        idPlusMap.put(R.id.buttonPlus6,6);
+        idPlusMap.put(R.id.buttonPlus7,7);
+        idPlusMap.put(R.id.buttonPlus8,8);
+        idPlusMap.put(R.id.buttonPlus9,9);
+        idPlusMap.put(R.id.buttonPlus10,10);
+        idPlusMap.put(R.id.buttonPlus11,11);
+        idPlusMap.put(R.id.buttonPlus12,12);
+        idPlusMap.put(R.id.buttonPlus13,13);
+        idPlusMap.put(R.id.buttonPlus14,14);
+        idPlusMap.put(R.id.buttonPlus15,15);
+        idPlusMap.put(R.id.buttonPlus16,16);
+        idPlusMap.put(R.id.buttonPlus17,17);
+        idPlusMap.put(R.id.buttonPlus18,18);
+        //map all the minus buttons.
+        idMinusMap.put(R.id.buttonMinus1,1);
+        idMinusMap.put(R.id.buttonMinus2,2);
+        idMinusMap.put(R.id.buttonMinus3,3);
+        idMinusMap.put(R.id.buttonMinus4,4);
+        idMinusMap.put(R.id.buttonMinus5,5);
+        idMinusMap.put(R.id.buttonMinus6,6);
+        idMinusMap.put(R.id.buttonMinus7,7);
+        idMinusMap.put(R.id.buttonMinus8,8);
+        idMinusMap.put(R.id.buttonMinus9,9);
+        idMinusMap.put(R.id.buttonMinus10,10);
+        idMinusMap.put(R.id.buttonMinus11,11);
+        idMinusMap.put(R.id.buttonMinus12,12);
+        idMinusMap.put(R.id.buttonMinus13,13);
+        idMinusMap.put(R.id.buttonMinus14,14);
+        idMinusMap.put(R.id.buttonMinus15,15);
+        idMinusMap.put(R.id.buttonMinus16,16);
+        idMinusMap.put(R.id.buttonMinus17,17);
+        idMinusMap.put(R.id.buttonMinus18,18);
+        //all the text views
+        idTWMap.put(1,R.id.Score1TW);
+        idTWMap.put(2,R.id.Score2TW);
+        idTWMap.put(3,R.id.Score3TW);
+        idTWMap.put(4,R.id.Score4TW);
+        idTWMap.put(5,R.id.Score5TW);
+        idTWMap.put(6,R.id.Score6TW);
+        idTWMap.put(7,R.id.Score7TW);
+        idTWMap.put(8,R.id.Score8TW);
+        idTWMap.put(9,R.id.Score9TW);
+        idTWMap.put(10,R.id.Score10TW);
+        idTWMap.put(11,R.id.Score11TW);
+        idTWMap.put(12,R.id.Score12TW);
+        idTWMap.put(13,R.id.Score13TW);
+        idTWMap.put(14,R.id.Score14TW);
+        idTWMap.put(15,R.id.Score15TW);
+        idTWMap.put(16,R.id.Score16TW);
+        idTWMap.put(17,R.id.Score17TW);
+        idTWMap.put(18,R.id.Score18TW);
+        //set the basic value of all the fields to "3"
+        for(Map.Entry<Integer, Integer> entry : idTWMap.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            TextView textView = findViewById(entry.getValue());
+            textView.setText("3");
+
+        }
+
 
         mDb = AppDatabase.getInMemoryDatabase(getApplicationContext());
 
@@ -53,16 +140,33 @@ public class MainActivity extends AppCompatActivity {
 
         fetchData();
     }
+    public void onClickMinusButton(View v) {
+        int n = idMinusMap.get(v.getId());
+        TextView textView = findViewById(idTWMap.get(n));
+        int i = Integer.parseInt((textView).getText().toString());
+        i -= 1;
+        textView.setText(String.valueOf(i));
+    }
 
     public void onClickPlusButton(View v) {
-        int i = Integer.parseInt(scoreTW1.getText().toString());
-        i++;
-        scoreTW1.setText(String.valueOf(i));
+        int n = idPlusMap.get(v.getId());
+            TextView textView = findViewById(idTWMap.get(n));
+            int i = Integer.parseInt(textView.getText().toString());
+            i++;
+            textView.setText(String.valueOf(i));
     }
-    public void onClickMinusButton(View v) {
-        int i = Integer.parseInt(scoreTW1.getText().toString());
-        i -= 1;
-        scoreTW1.setText(String.valueOf(i));
+    public void calculateTotal(View v) {
+        ArrayList<Integer> score = new ArrayList<>();
+        for(Map.Entry<Integer, Integer> entry : idTWMap.entrySet()) {
+            TextView textView = findViewById(entry.getValue());
+            int i = Integer.parseInt((textView).getText().toString());
+            score.add(i);
+        }
+        Double sum = new Double(0);
+        for (int i : score) {
+            sum = sum + i;
+        }
+        testTextView.setText(sum.toString());
     }
 
     private void populateDb() {
@@ -94,6 +198,7 @@ public class MainActivity extends AppCompatActivity {
         sB.append(String.format(Locale.ENGLISH,
                 "%s, %s (%d)\n", dbCourse.name, dbCourse.holesNumber,dbCourse.parNumber));
     }
+
 }
 
 
